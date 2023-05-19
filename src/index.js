a9bf4da8fdfc2646afb655f93337dd232d769e6f
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-05-19 11:57:18 +0300

    retrieve target job url from stored json file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const jobUrl = await axios.get('./jobPortals.json');
+const { my_job_portals } = await jobUrl.data;
+const { url } = my_job_portals[0];
+console.log(url);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
