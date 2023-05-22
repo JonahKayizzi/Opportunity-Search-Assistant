ea4fec0c8b6621981d4d118b81f7fa88aa6b4b1a
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-05-23 01:47:51 +0300

    Make http requests for every job portal url

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const fs = require('fs');
 const fetchData = async () => {
   try {
     const { myJobPortals } = JSON.parse(fs.readFileSync('./jobPortals.json'));
+    const urls = myJobPortals.map((job) => job.url);
+
+    for (const url of urls) {
+      const response = await axios.get(url);
+      const html = response.data;
+
+      console.log(body);
+    }
   } catch (error) {
     console.error('Error:', error);
   }
