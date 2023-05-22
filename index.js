230d54e90b8ce0576081fcb72fdcb8a595a2699e
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-05-23 01:45:48 +0300

    Read the JSON file using the fs module

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const cheerio = require('cheerio');
 const axios = require('axios');
+const fs = require('fs');
 
 const fetchData = async () => {
   try {
-    const jobUrl = await axios.get('./jobPortals.json');
-    const { myJobPortals } = await jobUrl.data;
-    const { url } = myJobPortals[0];
-
-    const response = await axios.get(url);
-    const html = response.data;
-
-    console.log(html);
-
-    const $ = cheerio.load(html);
-
-    const body = $('body').text().trim();
-
-    console.log(body);
+    const { myJobPortals } = JSON.parse(fs.readFileSync('./jobPortals.json'));
   } catch (error) {
     console.error('Error:', error);
   }
