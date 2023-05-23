1f6feb8731d116de42762a69daa0496de13e6df3
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-05-23 13:57:36 +0300

    Add a dummy test and testing configuration

---
+++ tests/index.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+// eslint-disable-next-line no-undef
+describe('sample test', () => {
+  // eslint-disable-next-line no-undef
+  it('return 1', () => {
+    const input = 1;
+    const result = 1;
+    assert.deepStrictEqual(input, result);
+  });
+});
