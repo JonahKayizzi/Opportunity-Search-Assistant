1c2e346555c6764b830e8256e7da2862dba1bfe7
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-05-29 01:52:53 +0300

    Update app logic to send email notification when there is a page update that matches they skills keywords

--- index.js
+++ index.js
@@ -53,7 +53,16 @@ const fetchData = async () => {
             console.log(`${job.name} has changed`);
             // eslint-disable-next-line no-unused-expressions
             keywords.some((keyword) => body.toLowerCase().includes(keyword))
-              ? (() => {})()
+              ? (() => {
+                  mailOptions.html = `<b>Visit their careers page ${job.url} to see the update</b>`;
+                  transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log(`Email sent: ${info.response}`);
+                    }
+                  });
+                })()
               : console.log('Not interested');
             job.content = body;
           })();
